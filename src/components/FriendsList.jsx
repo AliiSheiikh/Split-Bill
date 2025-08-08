d281f2162a93c07e787ea21ce99bf0cf38eeff81
Ali Sheikh <l_lawliet786@hotmail.com>
2025-08-07 22:12:28 -0400

    added friend component and rendering

---
+++ src/components/FriendsList.jsx
@@ -0,0 +1,13 @@
+import { initialFriends } from "../App.jsx";
+import Friend from "./Friend.jsx";
+export default function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
